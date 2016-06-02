69ad92219a68da3800ff3af445a703924c38706d
won <wjin0352@gmail.com>
2016-06-02 01:14:37 -0400

    added npm, frontend html, server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var Storage = function() {
+  this.items = [];
+  this.id = 0;
+};
+
+Storage.prototype.add = function(name) {
+  var item = {name: name, id: this.id};
+  this.items.push(item);
+  this.id += 1;
+  return item;
+};
+
+var storage = new Storage();
+storage.add('Broad beans');
+storage.add('Tomatoes');
+storage.add('Peppers');
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/items', function(req, res) {
+  res.json(storage.items);
+});
+
+app.listen(process.env.PORT || 8080);
