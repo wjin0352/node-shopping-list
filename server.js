cb9ff891dba3df8a0147d8c1b8a94963c0062c46
won <wjin0352@gmail.com>
2016-06-03 02:43:18 -0400

    completed put api endpoint

--- server.js
+++ server.js
@@ -14,13 +14,22 @@ Storage.prototype.add = function(name) {
   return item;
 };
 
-Storage.prototype.delete = function(id, item) {
+Storage.prototype.delete = function(id) {
   for(var i=0; i<this.items.length; i++) {
     if(this.items[i].id == id) {
       this.items.splice(i, 1);
       break;
     }
-  }
+  };
+};
+
+Storage.prototype.edit = function(id, name) {
+  for(var i=0; i<this.items.length; i++) {
+    if(this.items[i].id == id) {
+      this.items[i].name = name;
+      break;
+    }
+  };
 };
 
 var storage = new Storage();
@@ -47,21 +56,22 @@ app.use(express.static('public'));
 // If successful, your endpoint should return the deleted item, with the appropriate status code.
 // If an incorrect ID is supplied, your endpoint should fail gracefully, returning a JSON error message.
   app.delete('/items/:id', jsonParser, function(req, res) {
+    var id = req.params.id;
+    console.log('id: '+ id );
+    storage.delete(id);
+    res.sendStatus(204);
+  });
+
+// If successful, your endpoint should return the edited, with the appropriate status code.
+// If a non-existent ID is supplied, your endpoint should create a new item using the ID supplied.
+  app.put('/items/:id', jsonParser, function(req, res) {
     if (!req.body) {
       return res.sendStatus(400);
     }
     var id = req.params.id;
-    var item = storage.items.indexOf[id];
-    console.log('id: '+ id + ' item: ' + item );
-    storage.delete(id, item);
-    res.sendStatus(204);
-  });
-
-  app.put('items/:id', jsonParser, function(req, res) {
-    if (!req.body) {
-      return res.sendStatus(400);
-    }
-
+    var name = req.body.name;
+    storage.edit(id, name);
+    res.sendStatus(200);
   });
 
 app.listen(process.env.PORT || 8080);
