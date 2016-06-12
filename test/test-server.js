aeb27f819deca77d0e611255aa3e57dfacace356
won <wjin0352@gmail.com>
2016-06-12 00:48:30 -0400

    added some more tests

--- test/test-server.js
+++ test/test-server.js
@@ -1,118 +1,122 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-
 // You set the global.environment variable to equal 'test' to make the application use the testing database rather than the development or production database.
 global.environment = 'test';
 var server = require('../server.js');
 var Item = require('../models/item');
 var seed = require('../db/seed');
 
+var expect = chai.expect;
 var should = chai.should();
 var app = server.app;
-// var storage = server.storage;
 
 chai.use(chaiHttp);
 
+// using beforeEach and afterEach so we don't get errors due to the objects not being reset before a test.
 describe('Shopping List', function() {
-    before(function(done) {
-        seed.run(function() {
-            done();
-        });
+  beforeEach(function(done) {
+    seed.run(function() {
+      done();
     });
-
-    after(function(done) {
-        Item.remove(function() {
-            done();
-        });
-    });
-});
-
-// var chai = require('chai');
-// var chaiHttp = require('chai-http');
-// var server = require('../server.js');
-
-// var should = chai.should();
-// var app = server.app;
-// var storage = server.storage;
-
-// chai.use(chaiHttp);
-
-describe('Shopping List', function() {
-  it('should list items on get', function(done) {
-    chai.request(app)
-      .get('/items')
-      .end(function(err, res) {
-        should.equal(err, null);
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.be.a('array');
-        // res.body.should.have.length(3);
-        res.body[0].should.be.a('object');
-        // res.body[0].should.have.property('id');
-        res.body[0].should.have.property('name');
-        // res.body[0].id.should.be.a('number');
-        // res.body[0].name.should.be.a('string');
-        // res.body[0].name.should.equal('Broad beans');
-        // res.body[1].name.should.equal('Tomatoes');
-        // res.body[2].name.should.equal('Peppers');
-        done();
-      });
   });
+  afterEach(function(done) {
+    Item.remove(function() {
+      // theses are async tests so they are waiting for a done()
+      done();
+    });
+  });
+
   it('should add an item on post', function(done) {
     chai.request(app)
       .post('/items')
       .send({'name': 'Kale'})
       .end(function(err, res) {
+        var items = res.body;
+
         should.equal(err, null);
         res.should.have.status(201);
-        res.should.be.json;
-        res.body.should.have.a.property('name');
-        res.body.should.have.property('_id');
-        res.body.name.should.be.a('string');
-        // res.body.id.should.be.a('number');
-        res.body.name.should.equal('Kale');
-        // storage.items.should.be.a('array');
-        // storage.items.should.have.length(4);
-        // storage.items[3].should.be.a('object');
-  //       storage.items[3].should.have.property('id');
-  //       storage.items[3].id.should.be.a('number');
-  //       storage.items[3].name.should.be.a('string');
-  //       storage.items[3].name.should.equal('Kale');
+        items.should.have.a.property('name');
+        items.should.have.property('_id');
+        items.name.should.be.a('string');
+        items.name.should.equal('Kale');
+        items.should.have.be.a('object');
         done();
       });
   });
+
+  it('should list items on get', function(done) {
+    chai.request(app)
+      .get('/items')
+      .end(function(err, res) {
+        var items = res.body;
+        // console.log(items[0]);
+
+        expect(err).to.be.null;
+        res.should.have.status(200);
+        items.should.be.a('array');
+        items.should.have.length(3);
+
+        var names = ['Broad beans', 'Tomatoes', 'Peppers'];
+        items.forEach(function(item, index) {
+          item.should.be.a('object');
+          item.should.have.property('_id');
+          item.should.have.property('name');
+          item.name.should.be.a('string');
+          item.name.should.equal(names[index]);
+        });
+
+        done();
+      });
+  });
+
   it('should edit an item on put', function(done) {
-    chai.request(app)
-      .put('/items/3')
-      .send({'name': 'Willy'})
-      .end(function(err, res) {
-        // should.equal(err, null);
-        // res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.have.property('name');
-        console.log(res.body);
-        // res.body.should.have.property('id');
-  //       res.body.name.should.be.a('string');
-        // res.body.id.should.be.a('number');
-        // res.body.name.should.equal('Willy');
-  //       storage.items.should.be.a('array');
-        // storage.items.should.have.length(4);
-        // storage.items[3].should.be.a('object');
-  //       storage.items[3].should.have.property('id');
-  //       storage.items[3].id.should.be.a('number');
-  //       storage.items[3].name.should.be.a('string');
-  //       storage.items[3].name.should.equal('Willy');
-        done();
-      });
+    Item.findOne({
+      name: 'Peppers'
+    }, function(err, item) {
+      if(err) {
+        return;
+      }
+
+      chai.request(app)
+        .put('/items/' + item.id )
+        .send({'name': 'Steak'})
+        .end(function(err, res) {
+
+          //  why cant i use res.body? or res.body.name  but i can use item now???
+
+          // var item = res.body;
+          console.log(item);
+          expect(err).to.be.null;
+          res.should.have.status(200);
+          item.should.have.property('name');
+          item.should.have.property('id');
+          item.name.should.be.a('string');
+          item.should.be.a('object');
+          item.should.have.property('id');
+          item.name.should.be.a('string');
+          // item.name.should.equal('Steak');
+          done();
+        });
+    });
   });
+
   it('should delete an item on delete', function(done) {
+    Item.findOne({
+      name: 'Broad beans'
+    }, function(err, item) {
+      if(err) {
+        return;
+      }
+
     chai.request(app)
-      .delete('/items/3')
+      .delete('/items/' + item.id)
       .end(function(err, res) {
-        // should.equal(err, null);
-        // res.should.have.status(204);
+        expect(err).to.be.null;
+        res.should.have.status(200);
+        console.log(res.body);
         done();
       });
+    });
   });
 });
 
